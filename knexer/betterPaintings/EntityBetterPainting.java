0957e7c16868a088d1f5bed8c451f3307f92a663
Marjie <@>
2013-02-19 22:37:20 -0600

    Added class EntityBetterPainting

---
+++ knexer/betterPaintings/EntityBetterPainting.java
@@ -0,0 +1,27 @@
+package knexer.betterPaintings;
+
+import net.minecraft.entity.item.EntityPainting;
+import net.minecraft.util.EnumArt;
+import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class EntityBetterPainting extends EntityPainting {
+
+	public EntityBetterPainting(World world) {
+		super(world);
+	}
+
+	public EntityBetterPainting(World par1World, int par2, int par3, int par4, int par5) {
+		super(par1World, par2, par3, par4, par5);
+		// TODO
+		art = EnumArt.Aztec;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public EntityBetterPainting(World par1World, int par2, int par3, int par4,
+			int par5, String par6Str) {
+		super(par1World, par2, par3, par4, par5, par6Str);
+	}
+
+}
