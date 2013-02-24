9c12b0db9b3f4394039d7a2224af97c6d21107ba
Connor Schenck <raenottingham@gmail.com>
2013-02-24 15:08:03 -0600

    Fix in metadata: subtract 1 from width & height first (0-based)

--- knexer/betterPaintings/PaintingMetadataUtil.java
+++ knexer/betterPaintings/PaintingMetadataUtil.java
@@ -3,15 +3,15 @@ package knexer.betterPaintings;
 public class PaintingMetadataUtil {
 	
 	public static int getDamageForSize(int width, int height) {
-		return width + (height * 4);
+		return (width - 1) + ((height - 1) * 4);
 	}
 	
 	public static int getWidth(int damage) {
-		return damage % 4;
+		return (damage % 4) + 1;
 	}
 	
 	public static int getHeight(int damage) {
-		return damage / 4;
+		return (damage / 4) + 1;
 	}
 	
 	public static String getNameSuffix(int damage) {
