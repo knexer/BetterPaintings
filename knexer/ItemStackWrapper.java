fea8161546bbff94c5e465955ec790dfe44774f6
Taylor Bergquist <knexer@gmail.com>
2013-02-24 23:03:50 -0600

    Added ItemStackWrapper
    This utility class simply allows ItemStacks to be used in Java
    collections more easily.

---
+++ knexer/ItemStackWrapper.java
@@ -0,0 +1,36 @@
+package knexer;
+
+import net.minecraft.item.ItemStack;
+
+public class ItemStackWrapper {
+	private ItemStack wrapped;
+	
+	public ItemStackWrapper(ItemStack toWrap)
+	{
+		wrapped = toWrap;
+	}
+	
+	public ItemStack getWrappedItemStack()
+	{
+		return wrapped;
+	}
+	
+	/**
+	 * Checks this item for equality with other.
+	 * 
+	 * Compares itemID, stack size, and item damage.
+	 */
+	@Override
+	public boolean equals(Object other)
+	{
+		if(other == null) return false;
+		if(!(other instanceof ItemStackWrapper)) return false;
+		ItemStack o = (ItemStack)other;
+		
+		if(wrapped.itemID != o.itemID) return false;
+		if(wrapped.stackSize != o.stackSize) return false;
+		if(wrapped.getItemDamage() != o.getItemDamage()) return false;
+		
+		return true;
+	}
+}
